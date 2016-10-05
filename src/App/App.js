9335cc7fcbcb5d9e50df38d8f7d405c8cdd4a879
Fabian Buentello <fabian.buentello@gmail.com>
2016-10-05 17:44:35 -0500

    Successfully apply sidebar

--- src/App/App.js
+++ src/App/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import { Grid } from 'react-bootstrap';
-import DragulaComp from '../dragula/DragulaApp';
-import NavbarComp from '../components/Navbar/Navbar';
-import SideBar from '../components/SideBar/SideBar';
+import React from 'react';
+import Sidebar from 'react-sidebar';
+import Navbar from '../components/Navbar/Navbar';
+import Canvas from '../components/Canvas/Canvas';
+import SidebarContent from '../components/SideBar/SidebarContent';
 
+class App extends React.Component {
 
-class App extends Component {
   render() {
+    const sidebarProps = {
+      sidebar: (<SidebarContent />),
+      docked: true,
+      shadow: false
+    };
 
     return (
-      <div>
-        <NavbarComp/>
-          <SideBar/>
-        <Grid>
-          <DragulaComp/>
-        </Grid>
-      </div>
+      <Sidebar {...sidebarProps}>
+        <Navbar title="Talana"/>
+        <Canvas/>
+      </Sidebar>
     );
   }
 }
--- src/components/dragula/Draggable.js
+++ src/components/dragula/Draggable.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import * as ReactDOM from 'react-dom';
 import Dragula from 'react-dragula';
 import './dragula.css';
 
