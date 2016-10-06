a43e970d497962b5b7d746475ccb18500b6d7337
Fabian Buentello <fabian.buentello@gmail.com>
2016-10-06 08:47:08 -0500

    add draggable functionality

--- src/components/dragula/Draggable.js
+++ src/components/dragula/Draggable.js
@@ -1,23 +1,43 @@
 import * as React from "react";
+import { Grid, Row, Col } from 'react-bootstrap';
 import Dragula from 'react-dragula';
-import './dragula.css';
+import './draggable.css';
 
 export class DragulaComp extends React.Component {
   render () {
-    return <div className='dragula-section' ref={this.dragulaDecorator}>
-      <div>Swap me around</div>
-      <div>Swap her around</div>
-      <div>Swap him around</div>
-      <div>Swap them around</div>
-      <div>Swap us around</div>
-      <div>Swap things around</div>
-      <div>Swap everything around</div>
-    </div>;
+    return (
+        <div className="row dragula-section draggable draggableArea" ref={this.dragulaDecorator}>
+          <Col xs={2} className="draggable">Swap item #1</Col>
+          <Col xs={2} className="draggable">Swap item #2</Col>
+          <Col xs={2} className="draggable">Swap item #3</Col>
+          <Col xs={2} className="draggable">Swap item #4</Col>
+          <Col xs={2} className="draggable">Swap item #5</Col>
+          <Col xs={2} className="draggable">Swap item #6</Col>
+        </div>
+      );
+
   }
 
   dragulaDecorator = (componentBackingInstance) => {
     if (componentBackingInstance) {
-      let options = { };
+      let options = {
+        copy: function(el, source) {
+          // if source is widget picker we copy
+          if (source.classList.contains("allow_copy")) {
+            return true;
+          }
+        },
+        moves: function(el, container, handle) {
+          return el.classList.contains('draggableArea') || el.classList.contains('draggable');
+        },
+        accepts: function(el, target, source, sibling) {
+          // console.log("target.classList", sibling.classList);
+          return target.classList.contains("dragula-section");
+        },
+        direction: 'horizontal', // Y axis is considered when determining where an element would be dropped
+        // copySortSource: true,             // elements in copy-source containers can be reordered
+        removeOnSpill: true, // spilling will `.remove` the element, if this is true
+      };
       Dragula([componentBackingInstance], options);
     }
   };
